97b2808982435a3385f44eae754353b2fe919361
Alex Micu <alex.micu2012@yahoo.com>
2022-10-11 21:51:29 -0400

    finished with the logi, style is left to de

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,17 @@
-# a class
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+
+    question = params[:question]
+
+    @answers = if question.downcase == 'i am going to work'
+                 @answers.first
+               elsif question.end_with?('?')
+                 @answers[1]
+               else
+                 @answers.last
+               end
   end
 end
